9b36ff728a7d540b52987f1e21f1ae6c726d5b10
Srijita Mandal <84610348+Srijita-Mandal@users.noreply.github.com>
2022-09-24 20:44:14 +0530

    Create Sort an array of 0's 1's 2's.cpp

---
+++ Arrays/Sort an array of 0's 1's 2's.cpp
@@ -0,0 +1,17 @@
+Leetcode: https://leetcode.com/problems/sort-colors/
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(nums[j]>nums[j+1]){
+                    int temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                }
+            }
+        }
+    }
+};
